2c9c9e0249a2d7ef63f556fd19fcb238bb089710
jahenaod <68928387+jahenaod@users.noreply.github.com>
2020-09-03 19:56:55 -0500

    Add files via upload

---
+++ talleres/taller06/Abeja.java
@@ -0,0 +1,99 @@
+
+/**
+ * Write a description of class Abeha here.
+ * 
+ * @author Juan Andres Henao, Carlos Alexander Macias Mosquera 
+ * @version (a version number or a date)
+ */
+public class Abeja
+{
+       // instance variables - replace the example below with your own
+    private double x;
+    private double y;
+    private double z; //Altura
+    
+
+    /**
+     * Constructor for objects of class Abeja
+     */
+    public Abeja()
+    {
+        // initialise instance variables
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+    
+    /**
+     * Constructor for objects of class Abeja
+     */
+    public Abeja(double x, double y, double z)
+    {
+        // initialise instance variables
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * 
+     * @return     coordinate x
+     */
+    public double x()
+    {
+        // put your code here
+        return x;
+    }
+    
+    /**
+     * 
+     * @return     coordinate y
+     */
+    public double y()
+    {
+        // put your code here
+        return y;
+    }
+    
+    /**
+     *
+     * @return     coordinate z
+     */
+    public double z()
+    {
+        // put your code here
+        return z;
+    }
+    
+    /**
+     *
+     * @param   coordinate x
+     */
+    public void setX(double x)
+    {
+        // put your code here
+        this.x = x;
+    }
+    
+    /**
+     *
+     * @param   coordinate y
+     */
+    public void setY(double y)
+    {
+        // put your code here
+        this.y = y;
+    }
+    
+    /**
+     *
+     * @param   coordinate z
+     */
+    public void setZ(double z)
+    {
+        // put your code here
+        this.z = z;
+    }
+    
+
+}
